4c0dc6d7dbd25f7d1e28979f3e2f59a50a8b310b
Pello Xabier Altadill <pello_altadill@cuatrovientos.org>
2016-03-20 10:19:17 +0000

    Changes applied

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './myformsimple.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,54 +10,31 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, myformsimple_component_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (myformsimple_component_1_1) {
+                myformsimple_component_1 = myformsimple_component_1_1;
             }],
         execute: function() {
-            //import {AppComponent} from './app.button'
             /**
-             * class binding
-             * CSS binding
-             * events binding
-             * manual 2way binding
-             * angular2 2 way binding
+             * Form samples
              */
             // Different kinds of interpolation
             // Notice textContent is a dom element
             AppComponent = (function () {
                 function AppComponent() {
-                    this.title = 'Binding samples';
-                    this.topicText = 'Some samples for binding';
-                    this.isActive = true;
-                    this.buttonTitle = '';
-                    this.otherTitle = 'Two way binding';
-                    this.yetAnotherBind = 'yet another two way binding';
-                    this.heartOn = false;
+                    this.title = 'Form samples';
                 }
-                AppComponent.prototype.toggleCss = function () {
-                    this.isActive = !this.isActive;
-                };
-                AppComponent.prototype.toggleCssEvent = function ($event) {
-                    // Prevent Bubbling, in case that we are insie other element
-                    // expecting clic
-                    $event.stopPropagation();
-                    this.isActive = !this.isActive;
-                };
-                AppComponent.prototype.clearTitle = function ($event) {
-                    $event.preventDefault();
-                    this.buttonTitle = '';
-                };
-                AppComponent.prototype.heartBeat = function () {
-                    this.heartOn = !this.heartOn;
-                };
                 AppComponent = __decorate([
                     core_1.Component({
-                        selector: 'bindings-app',
-                        template: "<section>\n                <h1>{{title}}</h1>\n                <p [textContent]='topicText'></p>\n                <div><button class=\"btn btn-primary\" [class.active]=\"isActive\">Do something</button></div>\n                <div><button class=\"btn btn-primary\" \n                    [style.backgroundColor]=\"isActive ? 'green' : 'grey'\">CSS binding</button>\n\n                </div>\n                <div><button (click)=\"toggleCss()\">Toggle</button></div>\n                <div><button on-click=\"toggleCssEvent($event)\">Toggle</button></div>\n                <div>\n                    <input type=\"text\" [value]=\"buttonTitle\" (input)=\"buttonTitle = $event.target.value\" />\n                    <a href='' (click)=\"clearTitle($event)\">Clear</a>\n                    <b>{{buttonTitle}}</b>\n                </div>\n                <div>\n                    <input type=\"text\" [(ngModel)]=\"otherTitle\" />\n                    <i>{{otherTitle}}</i>\n                </div>\n                <div>\n                    <input type=\"text\" bindon-ngModel=\"yetAnotherBind\" />\n                    <i>{{yetAnotherBind}}</i>\n                </div>\n                <div>\n                    <h3><i class=\"glyphicon\"\n                       [class.glyphicon-heart-empty] = \"!heartOn\"\n                       [class.glyphicon-heart] = \"heartOn\"\n                       [style.color] = \"heartOn ? 'red' : 'none' \"\n                       (click)=\"heartBeat()\">\n                    </i> Beat Me!</h3>\n                </div>\n               </section>\n            "
+                        selector: 'forms-app',
+                        directives: [myformsimple_component_1.MyFormSimpleComponent],
+                        template: "<div>\n                {{title}}\n                <myform-simple></myform-simple>\n               </div>"
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,78 +1,19 @@
 import {Component} from 'angular2/core';
-//import {AppComponent} from './app.button'
+import {MyFormSimpleComponent} from './myformsimple.component';
 
 /**
- * class binding
- * CSS binding
- * events binding
- * manual 2way binding
- * angular2 2 way binding
+ * Form samples
  */
 // Different kinds of interpolation
 // Notice textContent is a dom element
 @Component({
-    selector: 'bindings-app',
-    template: `<section>
-                <h1>{{title}}</h1>
-                <p [textContent]='topicText'></p>
-                <div><button class="btn btn-primary" [class.active]="isActive">Do something</button></div>
-                <div><button class="btn btn-primary" 
-                    [style.backgroundColor]="isActive ? 'green' : 'grey'">CSS binding</button>
-
-                </div>
-                <div><button (click)="toggleCss()">Toggle</button></div>
-                <div><button on-click="toggleCssEvent($event)">Toggle</button></div>
-                <div>
-                    <input type="text" [value]="buttonTitle" (input)="buttonTitle = $event.target.value" />
-                    <a href='' (click)="clearTitle($event)">Clear</a>
-                    <b>{{buttonTitle}}</b>
-                </div>
-                <div>
-                    <input type="text" [(ngModel)]="otherTitle" />
-                    <i>{{otherTitle}}</i>
-                </div>
-                <div>
-                    <input type="text" bindon-ngModel="yetAnotherBind" />
-                    <i>{{yetAnotherBind}}</i>
-                </div>
-                <div>
-                    <h3><i class="glyphicon"
-                       [class.glyphicon-heart-empty] = "!heartOn"
-                       [class.glyphicon-heart] = "heartOn"
-                       [style.color] = "heartOn ? 'red' : 'none' "
-                       (click)="heartBeat()">
-                    </i> Beat Me!</h3>
-                </div>
-               </section>
-            `
+    selector: 'forms-app',
+    directives: [MyFormSimpleComponent],
+    template: `<div>
+                {{title}}
+                <myform-simple></myform-simple>
+               </div>`
 })
 export class AppComponent {
-    private title : string = 'Binding samples';
-    private topicText : string = 'Some samples for binding';
-    private isActive: boolean = true;
-    private buttonTitle : string = '';
-    private otherTitle : string = 'Two way binding';
-    private yetAnotherBind : string = 'yet another two way binding';
-    private heartOn : boolean = false;
-    
-    public toggleCss () : void {
-        this.isActive = !this.isActive;
-    }
-   
-   
-    public toggleCssEvent ($event) : void {
-        // Prevent Bubbling, in case that we are insie other element
-        // expecting clic
-        $event.stopPropagation();
-        this.isActive = !this.isActive;
-    } 
-    
-    public clearTitle ($event) {
-        $event.preventDefault();
-        this.buttonTitle = '';
-    }
-    
-    public heartBeat () {
-        this.heartOn = !this.heartOn;
-    }
+    private title : string = 'Form samples';
 }
