44a4b7683f30694912b9e6143e2060188b334f9d
pxai <pello_altadill@cuatrovientos.org>
2016-03-22 23:50:17 +0100

    Added Form Group sample

---
+++ app/formgroup.component.ts
@@ -0,0 +1,33 @@
+import {Component} from 'angular2/core';
+
+/**
+ * Form samples
+ * With this one we just check on console
+ * the value of a form control.
+ * There is no binding with the class
+ */
+@Component({
+    selector: 'myform-simple',
+    template: `<div>
+                {{title}}
+                <form >
+                    <div class="form-group">
+                        <label for="login">Login</label>
+                        <input type="text" ngControl="login" #login="ngForm" (change)="formChanged(login)" id="login" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" ngControl="password" #password="ngForm" (change)="formChanged(password)" id="password" class="form-control" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </form>
+               </div>`
+})
+export class MyFormSimpleComponent {
+    private title : string = 'Simple form';
+    
+    public formChanged (field) {
+        console.log(field);
+    }
+       
+}
