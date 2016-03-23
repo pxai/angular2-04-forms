74211262d965f4a35afde67f2d8789b76b1fca41
pelloxabier <pello.altadill@gmail.com>
2016-03-23 23:56:04 +0100

    rename control group

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,7 @@ import {MyFormSimpleComponent} from './myformsimple.component';
 import {SimpleValidationComponent} from './simplevalidation.component';
 import {SpecificValidationComponent} from './specificvalidation.component';
 import {NgFormValidationComponent} from './ngformvalidation.component';
-import {FormGroupComponent} from './formgroup.component';
+import {ControlGroupComponent} from './controlgroup.component';
 
 /**
  * Form samples
@@ -21,7 +21,7 @@ import {FormGroupComponent} from './formgroup.component';
                     <li><a [routerLink]="['SimpleValidation']">Simple form with validation</a></li>
                     <li><a [routerLink]="['SpecificValidation']">Form with Specific validation</a></li>
                     <li><a [routerLink]="['NgFormValidation']">Form with ngForm</a></li>
-                    <li><a [routerLink]="['FormGroupValidation']">Form with Form Group</a></li>
+                    <li><a [routerLink]="['ControlGroupValidation']">Form with Form Control Group</a></li>
                 </ul>
                 </nav>
                 <router-outlet></router-outlet>`
@@ -31,7 +31,7 @@ import {FormGroupComponent} from './formgroup.component';
   {path:'/simplevalidation', name: 'SimpleValidation', component: SimpleValidationComponent},
   {path:'/specificvalidation', name: 'SpecificValidation', component: SpecificValidationComponent},
   {path:'/ngformvalidation', name: 'NgFormValidation', component: NgFormValidationComponent},
-  {path:'/formgroupvalidation', name: 'FormGroupValidation', component: FormGroupComponent}
+  {path:'/controlgroupvalidation', name: 'ControlGroupValidation', component: ControlGroupComponent}
   ])
 export class AppComponent {
     private title : string = 'Form samples';
